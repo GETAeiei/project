f5872c20ea2d628f5933e937da0e6d337c7e5c56
kkob999 <68890159+kkob999@users.noreply.github.com>
2021-03-11 16:23:38 +0700

    Update dice.cpp

--- boardgameV2/dice.cpp
+++ boardgameV2/dice.cpp
@@ -1,8 +1,9 @@
 #include "dice.h"
-#include "bordgame.h"
+#include "main3.h"
 #include<cstdlib>
 #include<ctime>
 #include<iostream>
+
 Dice::Dice(){}
 Dice::~Dice(){}
 
@@ -10,4 +11,11 @@ int Dice::roll_dice(){
     srand(time(0));
     dice = rand() % 6 + 1;
     return dice;
+}
+
+int Dice::randChance()
+{
+    srand(time(0));
+    rand_chance = rand() % 10 + 1;
+    return rand_chance;
 }

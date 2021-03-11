8c5882c9407bb571dfdcabc72e3a1cd1574f3c20
kkob999 <68890159+kkob999@users.noreply.github.com>
2021-03-11 16:26:23 +0700

    Update chance.h

--- boardgameV2/chance.h
+++ boardgameV2/chance.h
@@ -1,5 +1,6 @@
 #ifndef CHANCE_H
 #define CHANCE_H
+#include "randchance.h"
 
 #include <QDialog>
 
@@ -14,9 +15,14 @@ class Chance : public QDialog
 public:
     explicit Chance(QWidget *parent = nullptr);
     ~Chance();
+    int random_chance;
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::Chance *ui;
+    Randchance *randchance;
 };
 
 #endif // CHANCE_H

b01b3855af7ef6a8d04d5f27218c1872d80b3070
Lapasrada-p <76034405+Lapasrada-p@users.noreply.github.com>
2021-03-18 00:10:35 +0700

    Update dice.h

--- monopenguin/dice.h
+++ monopenguin/dice.h
@@ -8,14 +8,7 @@ public:
 
     int dice;
     int roll_dice();
-    int rand_chance;
-    int randChance();
-    int a1;
-    int b1;
-    int a2;
-    int b2;
-    bool player1;
-
+    
 };
 
 #endif
